191f0593d302fe98bfaa0bb45e2719caa7e01407
Rob Heckart <rob.heckart@gmail.com>
2019-09-22 20:51:07 -0400

    Bug Fix and styles Changes
    Permissions shortened to not require any. Fixed bug where sometimes the animation didn't occur when switching regions. Changed classes so that only the text blinks, not the background. Ignores a region of Global.

--- aws-region.js
+++ aws-region.js
@@ -5,32 +5,45 @@ var currentRegion;
 var port = chrome.runtime.connect({ name: "aws_region" });
 port.postMessage({ command: "GetDefaultRegion" });
 
+function getRegionLabel() {
+  var regionArea = document.getElementById("nav-regionMenu");
+  var regionLabels = regionArea.getElementsByClassName("nav-elt-label");
+
+  if (regionLabels) {
+      return regionLabels[0];
+  }
+
+  return null;
+}
+
+function alertOnDifference() {
+  if (currentRegion && defaultRegion && (currentRegion !== 'Global') && currentRegion !== defaultRegion) {
+    var regionLabel = getRegionLabel();
+    regionLabel.className = regionLabel.className + " aws-wrong-region-warning";
+  }
+}
+
 port.onMessage.addListener(function(message, sender) {
   if (message.command && message.command === "DefaultRegion") {
     defaultRegion = message.response;
 
-    if (currentRegion && defaultRegion && currentRegion !== defaultRegion) {
-      var regionArea = document.getElementById("nav-regionMenu");
-      regionArea.className = regionArea.className + " aws-wrong-region-warning";
-    }
+    alertOnDifference();
   }
 });
 
 function defaultRegionCheck() {
-  var regionArea = document.getElementById("nav-regionMenu");
+  var regionLabel = getRegionLabel();
 
-  if (regionArea) {
-    var regionLabel = regionArea.getElementsByClassName("nav-elt-label");
+  if (regionLabel) {
+    var region = regionLabel.innerText;
+    currentRegion = region;
 
-    if (regionLabel) {
-      var region = regionLabel[0].innerText;
-      currentRegion = region;
+    alertOnDifference();
 
-      port.postMessage({
-        command: "SetCurrentRegion",
-        region: region
-      });
-    }
+    port.postMessage({
+      command: "SetCurrentRegion",
+      region: region
+    });
   }
 }
 
