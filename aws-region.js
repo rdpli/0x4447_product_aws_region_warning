aa8715ad94d791bf7bab07ca84510d56f3964f1c
David Gatti <github@gatti.email>
2019-10-19 10:56:32 +0200

    cleanup

--- aws-region.js
+++ aws-region.js
@@ -1,101 +1,125 @@
-var defaultRegion;
-var currentRegion;
+let defaultRegion;
+let currentRegion;
 
 //
-//    1.  This line opens up a long-lived connection to your background page.
+//  1.  This line opens up a long-lived connection to your background page.
 //
-var port = chrome.runtime.connect({ name: "aws_region" });
+let port = chrome.runtime.connect({ name: "aws_region" });
 port.postMessage({ command: "GetDefaultRegion" });
 
 //
-//    2.  This script resides with the AWS Console scripts. This function finds the current region on the Console page.
+//  2.  This script resides with the AWS Console scripts. This function
+//      finds the current region on the Console page.
 //
 function getRegionLabel() {
-  var regionArea = document.getElementById("nav-regionMenu");
 
-  if (!regionArea) {
+    let regionArea = document.getElementById("nav-regionMenu");
+
+    if(!regionArea)
+    {
+        return null;
+    }
+
+    let regionLabels = regionArea.getElementsByClassName("nav-elt-label");
+
+    if(regionLabels)
+    {
+        return regionLabels[0];
+    }
+
     return null;
-  }
-  
-  var regionLabels = regionArea.getElementsByClassName("nav-elt-label");
-
-  if (regionLabels) {
-      return regionLabels[0];
-  }
-
-  return null;
 }
 
 //
-//    3.   This function finds the region and compares the saved region to the current one, changing the CSS styles of the region if necessary.
+//  3.  This function finds the region and compares the saved region to the
+//      current one, changing the CSS styles of the region if necessary.
 //
 function alertOnDifference() {
-  var regionLabel = getRegionLabel();
 
-  if (currentRegion &&
-    defaultRegion &&
-    (currentRegion !== "Global") &&
-    currentRegion !== defaultRegion) {
-      regionLabel.className = regionLabel.className.replace(" aws-correct-region", "");
+    let regionLabel = getRegionLabel();
 
-      if (!regionLabel.className.includes("aws-wrong-region-warning")) {
+    if (currentRegion &&
+        defaultRegion &&
+        (currentRegion !== "Global") &&
+        currentRegion !== defaultRegion) {
+        regionLabel.className = regionLabel.className.replace(" aws-correct-region", "");
+
+    if (!regionLabel.className.includes("aws-wrong-region-warning")) {
         regionLabel.className = regionLabel.className + " aws-wrong-region-warning";
-      }
-  } else {    
-    regionLabel.className = regionLabel.className.replace(" aws-wrong-region-warning", "");
-    
-    if (!regionLabel.className.includes("aws-correct-region")) {
-      regionLabel.className = regionLabel.className + " aws-correct-region";
     }
-  }
+    } else {
+        regionLabel.className = regionLabel.className.replace(" aws-wrong-region-warning", "");
+
+        if (!regionLabel.className.includes("aws-correct-region")) {
+            regionLabel.className = regionLabel.className + " aws-correct-region";
+        }
+    }
+
 }
 
 //
-//    4.  Listens for local messages from the background script. If the degault region changes, this fires, does the comparison and changes the color of the region
+//  4.  Listens for local messages from the background script. If the
+//      degault region changes, this fires, does the comparison and changes
+//      the color of the region.
 //
 port.onMessage.addListener(function(message, sender) {
-  defaultRegionCheck();
 
-  if (message.command && message.command === "DefaultRegion") {
-    defaultRegion = message.response;
-    alertOnDifference();
-  }
+    defaultRegionCheck();
+
+    if(message.command && message.command === "DefaultRegion")
+    {
+        defaultRegion = message.response;
+        alertOnDifference();
+    }
+
 });
 
 //
-//    5.  Listens for messages sent to an active tab. If the degault region changes, this fires, does the comparison and changes the color of the region
+//  5.  Listens for messages sent to an active tab. If the degault region
+//      changes, this fires, does the comparison and changes the color of
+//      the region.
 //
 chrome.runtime.onMessage.addListener(function(message, sender) {
-  defaultRegionCheck();
 
-  if (message.command && message.command === "DefaultRegion") {
-    defaultRegion = message.response;
-    alertOnDifference();
-  }
+    defaultRegionCheck();
+
+    if(message.command && message.command === "DefaultRegion")
+    {
+        defaultRegion = message.response;
+        alertOnDifference();
+    }
+
 });
 
 //
-//    6. Gets the current region from the HTML and checks to see if the region is the same as saved or not. It also sends the current region to any listeners.
+//  6.  Gets the current region from the HTML and checks to see if the region
+//      is the same as saved or not. It also sends the current region to
+//      any listeners.
 //
 function defaultRegionCheck() {
-  var regionLabel = getRegionLabel();
 
-  if (regionLabel) {
-    var region = regionLabel.innerText;
-    currentRegion = region;
+    let regionLabel = getRegionLabel();
 
-    alertOnDifference();
+    if(regionLabel)
+    {
+        let region = regionLabel.innerText;
 
-    port.postMessage({
-      command: "SetCurrentRegion",
-      region: region
-    });
-  }
+        currentRegion = region;
+
+        alertOnDifference();
+
+        port.postMessage({
+            command: "SetCurrentRegion",
+            region: region
+        });
+    }
 }
 
 //
-//    7. Runs the check above when the page has fully loaded
+//  7. Runs the check above when the page has fully loaded.
 //
 docReady(function() {
-  defaultRegionCheck();  
+
+    defaultRegionCheck();
+
 });
--- background.js
+++ background.js
@@ -1,47 +1,64 @@
-var currentRegion;
+//
+//  This file is responsible for ...
+//
 
+//
+//  1.  Start a variable which will hold the current region that we are on?
+//
+let currentRegion;
+
+//
+//  2.  This listener will react to ...
+//
 chrome.runtime.onConnect.addListener(function(port) {
-  port.postMessage({ greeting: "hello" });
 
-  //
-  //    1. Listen for messages on the chrome port from both the popup and the aws-region.js.
-  //
-  port.onMessage.addListener(function(message, sender) {
-    
-    // Which script send me the message?
-    switch (sender.name) {
-      case "aws_region":
+    //
+    //  1.  We post a message because ...
+    //
+    port.postMessage({ greeting: "hello" });
+
+    //
+    //  2. Listen for messages on the chrome port from both the popup
+    //      and the aws-region.js.
+    //
+    port.onMessage.addListener(function(message, sender) {
+
+    //
+    //  Check which file sent a message to the background process.
+    //
+    switch(sender.name) {
+        case "aws_region":
         switch (message.command) {
-          case "GetDefaultRegion":
+            case "GetDefaultRegion":
             getDefaultRegion(port);
             break;
 
-          case "SetCurrentRegion":
+            case "SetCurrentRegion":
             currentRegion = message.region;
             break;
         }
 
         break;
 
-      case "aws_region_popup":
+        case "aws_region_popup":
         if (!message || !message.command) return;
 
         switch (message.command) {
-          case "GetDefaultRegion":
+            case "GetDefaultRegion":
             getDefaultRegion(port);
 
             break;
 
-          case "SetDefaultRegion":
+            case "SetDefaultRegion":
             setDefaultRegion(message.region, port);
             sendContentSavedRegion(message.region);
 
             break;
 
-          case "GetCurrentRegion":
+            case "GetCurrentRegion":
             port.postMessage({
-              command: "CurrentRegion",
-              response: currentRegion
+                command: "CurrentRegion",
+                response: currentRegion
             });
 
             break;
@@ -49,70 +66,120 @@ chrome.runtime.onConnect.addListener(function(port) {
 
         break;
     }
-  });
+    });
 });
 
 //
-//    2.  When the extension first starts, set it so that it only listens and injects code into AWS comsole page
+//  2.  When the extension first starts, set it so that it only listens and
+//      injects code into AWS console page.
 //
 chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: "aws.amazon.com" }
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
+
+    //
+    //  Replace all rules ...
+    //
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+
+        //
+        //  With a new rule ...
+        //
+        chrome.declarativeContent.onPageChanged.addRules(
+            [
+                {
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { urlContains: "aws.amazon.com" }
+                        })
+                    ],
+                    //
+                    //  And shows the extension's page action.
+                    //
+                    actions: [new chrome.declarativeContent.ShowPageAction()]
+                }
+            ]
+        );
+
+    });
+
 });
 
 //
-//    3. Grabs the default region from local storage and posts a message to return it to the necessary script that called for it
+//  3.  Grabs the default region from local storage and posts a message to
+//      return it to the necessary script that called for it.
 //
 function getDefaultRegion(port) {
-  chrome.storage.sync.get("DefaultRegion", function(items) {
-    if (items) {
-      port.postMessage({
-        command: "DefaultRegion",
-        response: items.DefaultRegion
-      });
-    } else {
-      return false;
-    }
-  });
+
+    chrome.storage.sync.get("DefaultRegion", function(items) {
+
+        if(items)
+        {
+            port.postMessage({
+                command: "DefaultRegion",
+                response: items.DefaultRegion
+            });
+        }
+        else
+        {
+            return false;
+        }
+
+    });
 }
 
 //
-//    4. Saves the default region that the user chose to local browser storage.
+//  4.  Saves the default region that the user chose to local browser storage.
 //
 function setDefaultRegion(region, port) {
-  chrome.storage.sync.set({ DefaultRegion: region }, function() {
-    port.postMessage({ 
-      command: "DefaultRegion",
-      response: region });
-  });  
+
+    //
+    //  1.  Create configuration.
+    //
+    let params = {
+        DefaultRegion: region
+    };
+
+    //
+    //  2.  Send a message to set the new region?
+    //
+    chrome.storage.sync.set(params, function() {
+
+        port.postMessage({
+            command: "DefaultRegion",
+            response: region
+        });
+
+    });
 }
 
 //
-//    5. This function sends a tab-wide message to any open tab in the current window that has the AWS console open, with the payload being the default region.
+//  5.  This function sends a tab-wide message to any open tab in the current
+//      window that has the AWS console open, with the payload being the
+//      default region.
 //
 function sendContentSavedRegion(region) {
-  chrome.tabs.query({
-    currentWindow: true,
-    url: "*://*.aws.amazon.com/*"
-  }, function(tabs) {
-    tabs.forEach(function(tab) {
-      chrome.tabs.sendMessage(tab.id, {
-        command: "DefaultRegion",
-        response: region
-      });
+
+    //
+    //  1.  Create configuration.
+    //
+    let params = {
+        currentWindow: true,
+        url: "*://*.aws.amazon.com/*"
+    }
+
+    //
+    //  2.  We query what?
+    //
+    chrome.tabs.query(params, function(tabs) {
+
+        tabs.forEach(function(tab) {
+
+            chrome.tabs.sendMessage(tab.id, {
+                command: "DefaultRegion",
+                response: region
+            });
+
+        });
+
     });
-  });
+
 }
